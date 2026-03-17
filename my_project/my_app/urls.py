e138b1c7751de3c8a951b4a8ed4b14a346b59d65
Nipesh <nipeshincbca2021@gmail.com>
2026-03-17 10:00:33 +0545

    add and fetch data commit

--- my_project/my_app/urls.py
+++ my_project/my_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views.main_view import index_method,about_method
+from .views.main_view import index_method,about_method,add_product,edit_product
 
 urlpatterns = [
     path('', index_method, name="index"),
-    path('about/', about_method, name="about")
+    path('about/', about_method, name="about"),
+    path('add/', add_product, name="add_product"),
+    path('edit/',edit_product, name="edit")
 ]
--- my_project/my_app/views/main_view.py
+++ my_project/my_app/views/main_view.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render,redirect
+from ..models import Product
 
 def index_method(request):
-    return render(request,'main/index.html')
+    product =  Product.objects.all()
+    return render(request,'main/index.html',{'data':product})
+
+
+def add_product(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        category = request.POST.get('category')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+
+        product = Product.objects.create(
+            title=title,
+            category=category,
+            price = price,
+            description = description
+        )
+        product.save()
+        return redirect('index')
+
+    return render(request,'main/add_product.html')
+
+def edit_product(request):
+    return render(request,'main/edit_product.html')
+
 
 def about_method(request):
     return render(request,'main/about.html')
